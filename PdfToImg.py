fae2c6f2ffb82bd2dca96e489b8b3def258bd6c4
BOUNABIMOUAD <bounabimouad@gmail.com>
2017-09-21 17:30:10 +0200

    code fixed

--- PdfToImg.py
+++ PdfToImg.py
@@ -7,12 +7,11 @@ Created on Thu Sep 21 13:29:22 2017
 
 from __future__ import print_function
 from wand.image import Image
-from wand.color import Color
+
 
  
 with Image(filename='source.pdf',resolution=200) as img:
         print('pages = ', len(img.sequence))
-        img.colorspace = 'rec709luma'
         img.compression_quality = 99
         with img.convert('png') as converted:
             converted.save(filename='result/page_n.png')
